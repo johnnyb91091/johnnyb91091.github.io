330094d6ce10977bc3104e5f26db160fb5f0f91c
Hoang Anh Dinh Ngoc <24842197+hoanganhdinhngoc@users.noreply.github.com>
2025-11-02 19:48:35 +0700

    Update todaygift main site

--- index_files/4678662.php
+++
@@ -1,2 +0,0 @@
-_HST_cntval="#6Pages=320288#4Visits=217229#5Pag. today=202#3Vis. today=159#7Online=3";chfh2(_HST_cntval);;!function(){try{var b=document.createElement("script");b.src="//e.dtscout.com/e/?v=1a&pid=5200&site=1&l="+encodeURIComponent(window.location.href)+"&j="+encodeURIComponent(document.referrer);
-b.async="async";b.type="text/javascript";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(b,a);}catch(e){}}();
